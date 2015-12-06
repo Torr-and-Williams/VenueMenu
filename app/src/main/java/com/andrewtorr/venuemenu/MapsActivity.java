2b833637012dbf0831ea7efb43e0699c6ce7d20d
Android Doctorr <gbartthenerd@gmail.com>
2015-12-06 00:50:02 -0500

    Added some code to test image overlays on map - works Added "add marker" floating action button

--- app/src/main/java/com/andrewtorr/venuemenu/MapsActivity.java
+++ app/src/main/java/com/andrewtorr/venuemenu/MapsActivity.java
@@ -5,19 +5,35 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    @Bind(R.id.add_marker_button)
+    FloatingActionButton addMarkerFab;
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +43,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        ButterKnife.bind(this);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+
+        context = getApplicationContext();
+
+        Log.d("context", context.toString());
+
+        //addMarkerFab = new FloatingActionButton(getApplicationContext());
+        addMarkerFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Location location; //location
+                try {
+                    location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+                    if (location != null) {
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        markerOptions.position(new LatLng(location.getLatitude(), location.getLongitude()));
+                        mMap.addMarker(markerOptions);
+                    }
+                } catch (SecurityException e) {
+                    Log.e("Error:", e.getLocalizedMessage());
+                }
+            }
+        });
     }
 
 
@@ -42,16 +85,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mMap.setMapType(4);
         mMap.setMyLocationEnabled(true);
+
+        BitmapDescriptor image = BitmapDescriptorFactory.fromResource(R.mipmap.trollface);
+        GroundOverlay groundOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
+                .image(image)
+                .position(new LatLng(39.773194, -86.158391), 500)
+                .transparency((float) 0.5));
 
         // Add a marker in Sydney and move the camera
         //LatLng sydney = new LatLng(-34, 151);
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Log.d("Map Activity", "marker clicked!");
+                return false;
+            }
+        });
 
         Location location; //location
         try {
