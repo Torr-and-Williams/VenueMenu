3912dc0b62c587a2bd75950087786ff4affb25b0
Android Doctorr <gbartthenerd@gmail.com>
2016-01-11 02:13:39 -0500

    Created Layer Edit Menu Added TODOs for Layer editing Created Icon Changed app name

--- app/src/main/java/com/andrewtorr/venuemenu/MapsActivity.java
+++ app/src/main/java/com/andrewtorr/venuemenu/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.andrewtorr.venuemenu;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.location.Criteria;
 import android.location.Location;
@@ -8,7 +9,14 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.andrewtorr.venuemenu.Models.Waypoint;
@@ -28,16 +36,26 @@ import com.parse.SaveCallback;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     @Bind(R.id.add_marker_button)
     FloatingActionButton addMarkerFab;
+    @Bind(R.id.edit_layer_button)
+    FloatingActionButton editLayerFab;
+    @Bind(R.id.add_lot_button)
+    FloatingActionButton addLotButton;
 
     private GoogleMap mMap;
     private LocationManager locationManager;
     private Criteria criteria;
     private Context context;
 
+    private boolean leave = true;
+    private boolean mainBtns = true;
+    private boolean overlayMode = false;
+    private boolean pathMode = false;
+    private boolean lotMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,40 +77,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //Set Location Manager
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         criteria = new Criteria();
-
-
-        //addMarkerFab = new FloatingActionButton(getApplicationContext());
-        addMarkerFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Location location;
-
-                try {
-                    location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
-
-                    if (location != null) {
-                        //Make a new waypoint
-                        //TODO: Throw up confirmation dialog before saving - set name, upload image etc there
-                        Waypoint waypoint = new Waypoint("Test", location.getLatitude(), location.getLongitude());
-
-                        waypoint.saveInBackground(new SaveCallback() {
-                            @Override
-                            public void done(ParseException e) {
-                                Toast.makeText(context, "Waypoint saved!", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-
-                        MarkerOptions markerOptions = new MarkerOptions();
-                        markerOptions.position(new LatLng(location.getLatitude(), location.getLongitude()));
-                        mMap.addMarker(markerOptions);
-
-                        zoomInOnMe();
-                    }
-                } catch (SecurityException e) {
-                    Log.e("Error:", e.getLocalizedMessage());
-                }
-            }
-        });
     }
 
 
@@ -142,6 +126,76 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         zoomInOnMe();
     }
 
+
+
+
+
+
+    //Buttons
+
+    @OnClick(R.id.add_marker_button)
+    public void addMarker() {
+        Location location;
+
+        try {
+            location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+
+            if (location != null) {
+                //Make a new waypoint
+                //TODO: Throw up confirmation dialog before saving - set name, upload image etc there
+                Waypoint waypoint = new Waypoint("Test", location.getLatitude(), location.getLongitude());
+
+                waypoint.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Toast.makeText(context, "Waypoint saved!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(new LatLng(location.getLatitude(), location.getLongitude()));
+                mMap.addMarker(markerOptions);
+
+                zoomInOnMe();
+            }
+        } catch (SecurityException e) {
+            Log.e("Error:", e.getLocalizedMessage());
+        }
+    }
+
+    @OnClick(R.id.edit_layer_button)
+    public void editLayer() {
+        addMarkerFab.setVisibility(View.INVISIBLE);
+        editLayerFab.setVisibility(View.INVISIBLE);
+
+        showMenu();
+    }
+
+    @OnClick(R.id.add_lot_button)
+    public void addLot() {
+        //TODO: Make a canvas "visible", set up a touch listener to draw a point when the touch ends
+        //(Use editImageView in Do as a template)
+
+        //TODO: Set up an on touch listener for the point, moves while touching?
+        //TODO: OR set up touch listener to check to see if it's near a point at the beginning? - that sounds more complicated
+
+        //TODO: Add points to an array list
+
+        //TODO: Draw lines between all the points in order
+
+        //TODO: After at least two points have been drawn, allow snapping to the first point to complete the shape
+
+        //TODO: Prevent the lines from crossing somehow???
+
+
+        //TODO: If you click on an existing point on an existing lot, it should set the first point for the new lot there
+    }
+
+
+
+
+    //Functions
+
     public void zoomInOnMe() {
         Location location; //location
         try {
@@ -161,5 +215,153 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         } catch (SecurityException e) {
             Log.e("Error:", e.getLocalizedMessage());
         }
+    }
+
+    public void goFlat() {
+        Location location; //location
+        try {
+            location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+            if (location != null) {
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                        new LatLng(location.getLatitude(), location.getLongitude()), 13));
+
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
+                        .zoom(20)                   // Sets the zoom
+                        .bearing(90)                // Sets the orientation of the camera to east
+                        .tilt(0)                   // Sets the tilt of the camera
+                        .build();                   // Creates a CameraPosition from the builder
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        } catch (SecurityException e) {
+            Log.e("Error:", e.getLocalizedMessage());
+        }
+    }
+
+    public void showMenu() {
+        leave = true;
+        Log.d("Main Activity", "Adding menu view");
+
+        final Dialog dialog = new Dialog(MapsActivity.this, R.style.Dialog_Blank);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        LayoutInflater dialog_inflater = LayoutInflater.from(getApplicationContext());
+        final View dialog_view = dialog_inflater.inflate(R.layout.menu_holder, null);
+
+        final ViewGroup holder = (ViewGroup) dialog_view.findViewById(R.id.menu_holder);
+        final ViewGroup newView = (ViewGroup) LayoutInflater.from(this).inflate(
+                R.layout.bottom_menu, holder, false);
+
+        TranslateAnimation animateIn = new TranslateAnimation(0,0,256,0);
+        animateIn.setDuration(250);
+        newView.setAnimation(animateIn);
+        holder.addView(newView, 0);
+
+        final TextView button1 = (TextView) dialog_view.findViewById(R.id.button1);
+        final TextView button2 = (TextView) dialog_view.findViewById(R.id.button2);
+        final TextView button3 = (TextView) dialog_view.findViewById(R.id.button3);
+        final TextView button4 = (TextView) dialog_view.findViewById(R.id.button4);
+
+        final ImageButton topView = (ImageButton) dialog_view.findViewById(R.id.top_view);
+
+        final TranslateAnimation animateOut = new TranslateAnimation(0, 0, 0, 256);
+        animateOut.setDuration(250);
+        newView.setAnimation(animateOut);
+
+        View.OnClickListener m_clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(final View p_v) {
+                switch (p_v.getId()) {
+                    case R.id.button1:
+                        Log.d("Main Activity", "add marker button");
+
+                        leave = false;
+                        mainBtns = true;
+                        addMarker();
+                        break;
+
+                    case R.id.button2:
+                        Log.d("Main Activity", "set lot button");
+
+                        mainBtns = false;
+                        lotMode = true;
+                        break;
+
+                    case R.id.button3:
+                        Log.d("Main Activity", "set pathnet button");
+
+                        mainBtns = false;
+                        pathMode = true;
+                        break;
+
+                    case R.id.button4:
+                        Log.d("Main Activity", "set overlay button");
+
+                        goFlat();
+                        mainBtns = false;
+                        overlayMode = true;
+                        break;
+
+                    case R.id.top_view:
+                        mainBtns = true;
+                        break;
+
+                    default:
+                        mainBtns = true;
+                        break;
+                }
+
+                if (leave) {
+                    newView.startAnimation(animateOut);
+
+                    animateOut.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {}
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            if (mainBtns) {
+                                addMarkerFab.setVisibility(View.VISIBLE);
+                                editLayerFab.setVisibility(View.VISIBLE);
+                            }
+
+                            dialog.cancel();
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {}
+                    });
+                }
+            }
+        };
+
+        button1.setOnClickListener(m_clickListener);
+        button2.setOnClickListener(m_clickListener);
+        button3.setOnClickListener(m_clickListener);
+        button4.setOnClickListener(m_clickListener);
+
+        topView.setOnClickListener(m_clickListener);
+        dialog.setContentView(dialog_view);
+
+        newView.startAnimation(animateIn);
+        dialog.show();
+    }
+
+    private void editPaths() {
+        //TODO: Set up system similar to Lots
+
+    }
+
+    private void editLots() {
+        addLotButton.setVisibility(View.VISIBLE);
+
+        //TODO: Set up onclick listeners on existing lots to edit attributes (change image, title, description, etc.)
+        //Later: add ability to set Lat & Lng explicitly for the corners
+
+        //TODO: Set up long click listeners to edit vertices
+    }
+
+    private void editOverlays() {
+
     }
 }
